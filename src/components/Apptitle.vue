608e4dc39a2d64b1cf3ea535fc6c977db8ee3c0d
Yari <narducciyarigmail.com>
2022-12-07 17:52:03 +0100

    first commit

---
+++ src/components/Apptitle.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+
+    export default {
+        data (){
+            return{
+                saluti:"Si .... PUOOO....FAREEEEEEEEEEEEEE",
+                
+            }
+        }
+       
+    }
+
+
+
+
+</script>
+
+<template>
+ 
+  <h1>{{saluti}}</h1>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+h1{
+    background-color: brown;
+    color: blanchedalmond;
+}
+
+</style>
